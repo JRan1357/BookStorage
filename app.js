63d449f64a3254ab8747cca4eef72eaebd329a63
Jake <jakerandall1766@gmail.com>
2025-05-01 11:03:21 +0100

    working on reading form

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ function Book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }   
+
+
+function getValues() {
+    let inputs = ['title', 'author', 'pages', 'read']
+    document.getElementById('title').value new Book()
+    inputs.forEach(item => {
+        
+    })
+}
